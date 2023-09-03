c2bbfe3269b8d700322d82ff4bb83092383a8734
Korchagin <rag.slava@mail.ru>
2023-09-04 01:17:41 +0300

    Нахождение минимума в массиве

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int res;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int num1 = scanner.nextInt();
-        System.out.println("Введите второе число: ");
-        int num2 = scanner.nextInt();
-        scanner.nextLine(); // Считываем символ новой строки после ввода второго числа
-
-        System.out.print("Введите оператор действия: ");
-        String symbol = scanner.nextLine(); // Считываем оператор как строку
-
-        if (symbol.equals("+")) {
-            res = num1 + num2;
-            System.out.println(num1 + " + " + num2 + " = " + res);
-        } else if (symbol.equals("-")) {
-            res = num1 - num2;
-            System.out.println(num1 + " - " + num2 + " = " + res);
-        } else if (symbol.equals("*")) {
-            res = num1 * num2;
-            System.out.println(num1 + " * " + num2 + " = " + res);
-        } else if (symbol.equals("/") && num2 != 0) {
-            res = num1 / num2;
-            System.out.println(num1 + " / " + num2 + " = " + res);
-        } else {
-            System.out.println("Некорректный оператор или деление на ноль.");
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++){
+            System.out.println("Введите число в массив");
+            arr[i] = scanner.nextInt();
         }
-
-
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++){
+            System.out.print("[" + arr[i] + "], ") ;
+        }
+        System.out.println(" Минимальное занчение: " + min);
 
 
     }
