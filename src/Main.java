c97e0991f2ec556f7476d3e391b985ec21c8f392
Korchagin <rag.slava@mail.ru>
2023-09-04 21:50:35 +0300

    Изучил функции

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> numbersArray = new ArrayList<>();
-        LinkedList<Integer> numbersLinked = new LinkedList<>();
-        numbersArray.add(3);
-        numbersArray.add(4);
-        numbersArray.add(5);
-        numbersArray.add(6);
-        numbersArray.add(7);
-        numbersLinked.add(10);
-        numbersLinked.add(11);
-        numbersLinked.add(12);
-        numbersLinked.add(13);
-        numbersLinked.add(14);
-//        for (Integer el : numbersLinked) {
-//            System.out.print(el + ", ");
-//        }
-//        for (Integer el : numbersArray) {
-//            System.out.print(el + ", ");
-//        }
-        for (int i = numbersLinked.size() - 1; i >= 0; i--){
-            System.out.println(numbersLinked.get(i));
+    int[] leng = new int[]{1, 2, 3, 4, 5, 6};
+    int res = sumArray(leng);
+        System.out.println(res);
+    }
+    public static int sumArray(int[] arr){
+        int sum = 0;
+        for(int el : arr){
+            sum+=el;
         }
+        return sum;
     }
 }
 
